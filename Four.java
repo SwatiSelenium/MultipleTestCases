bab344f2a7bcf7f8d8e17ce86827a9988517c65c
Swati <swati.bhorkhade@gmail.com>
2023-12-22 15:25:15 +0530

    bySK

---
+++ Four.java
@@ -0,0 +1,13 @@
+package MultipleTestCases;
+
+import org.testng.annotations.Test;
+
+public class Four 
+{
+	@Test
+	public void testcase4()	
+	{
+		System.out.println("4");
+	}
+
+}
---
+++ One.java
@@ -0,0 +1,12 @@
+package MultipleTestCases;
+
+import org.testng.annotations.Test;
+
+public class One 
+{
+	@Test
+	public void testcase1()
+	{
+		System.out.println("1");
+	}
+}
